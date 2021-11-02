e1d88c14e8f0605826d63c00f7d97296e618d937
wino03 <wino229@gmail.com>
2021-11-02 14:29:47 +0000

    tugas test1 wiwarno

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => env('DB_HOST', '10.12.12.104'),
+            'port' => env('DB_PORT', '6033'),
+            'database' => env('DB_DATABASE', 'apps2'),
+            'username' => env('DB_USERNAME', 'apps'),
+            'password' => env('DB_PASSWORD', 'bismillah'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
